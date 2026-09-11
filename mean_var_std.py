93666fab545fe93ac227e5cca479f8ead03ea068
Daffa Afaf Firmansyah <daffaafaffirmansyah@gmail.com>
2026-09-11 20:25:55 +0700

    Add files via upload

---
+++ mean_var_std.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    # Ubah list menjadi array 3x3
+    matrix = np.array(list).reshape(3, 3)
+    
+    calculations = {
+        'mean': [
+            matrix.mean(axis=0).tolist(),   # mean per kolom
+            matrix.mean(axis=1).tolist(),   # mean per baris
+            matrix.mean().tolist()          # mean keseluruhan
+        ],
+        'variance': [
+            matrix.var(axis=0).tolist(),
+            matrix.var(axis=1).tolist(),
+            matrix.var().tolist()
+        ],
+        'standard deviation': [
+            matrix.std(axis=0).tolist(),
+            matrix.std(axis=1).tolist(),
+            matrix.std().tolist()
+        ],
+        'max': [
+            matrix.max(axis=0).tolist(),
+            matrix.max(axis=1).tolist(),
+            matrix.max().tolist()
+        ],
+        'min': [
+            matrix.min(axis=0).tolist(),
+            matrix.min(axis=1).tolist(),
+            matrix.min().tolist()
+        ],
+        'sum': [
+            matrix.sum(axis=0).tolist(),
+            matrix.sum(axis=1).tolist(),
+            matrix.sum().tolist()
+        ]
+    }
+    
+    return calculations
